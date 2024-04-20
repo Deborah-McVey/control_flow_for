5accc8d19c6cc119d9f517ea59fa2ae3144a59b1
Deborah McVey <deborah.mcvey@gmail.com>
2024-04-20 15:08:37 -0400

    for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'control_flow_for';
+
+  productList = [
+    {id: 1, name: 'I Phone'},
+    {id: 2, name: 'I Pad'},
+    {id: 3, name: 'Macbook'},
+    {id: 4, name: 'HP Laptop'},
+    {id: 5, name: 'Samsung TV'}
+  ];
 }
